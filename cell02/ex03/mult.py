58f333e1eb0d1a649376c4496e6b4e989bbced2d
perl <perlsuaymark@gmail.com>
2025-09-16 00:44:34 +0700

    Cell02 ex03

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+num1 = int(input("Enter the first number:\n"))
+num2 = int(input("Enter the second number:\n"))
+ans = num1 * num2
+
+print(f"{num1} x {num2} = {ans}")
+
+if(ans < 0):
+    print("The result is negative.")
+elif(ans > 0):
+    print("The result is positive.")
+else:
+    print("The result is positive and negative.")
