28d23ff8a4d75ce8f76d7ea5646e773fdd16efca
perl <perlsuaymark@gmail.com>
2025-09-16 22:23:18 +0700

    Cell03 ex03 re

--- cell03/ex03/advanced_mult.py
+++ cell03/ex03/advanced_mult.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
-
-for i in range(0, 11, 1):
+i = 0
+j = 0
+while(i < 11):
     print(f"teble de {i}:", end = " ")
-    for j in range(0, 11, 1):
+    j = 0
+    while(j < 11):
         print(i * j, end = " ")
+        j=j+1
     print("")
+    i=i+1
