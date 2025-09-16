e0eefce4267cdf30f878c187861dc394e62737c0
perl <perlsuaymark@gmail.com>
2025-09-16 22:15:40 +0700

    Cell 03 ex02,03

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+resp = input("What you gotta say? : ")
+
+while(resp != "STOP"):
+    resp = input("I got that! Anything else? : ")
+    
---
+++ cell03/ex03/advanced_mult.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+for i in range(0, 11, 1):
+    print(f"teble de {i}:", end = " ")
+    for j in range(0, 11, 1):
+        print(i * j, end = " ")
+    print("")
